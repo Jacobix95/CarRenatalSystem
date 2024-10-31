bff66addcfe7c14b1d4218be61c6788ec98533a5
Jakub <kuba.reszel@gmail.com>
2024-10-31 16:43:16 +0100

    add Car class and lombok

---
+++ src/main/java/com/CarRentalSystem/Car.java
@@ -0,0 +1,21 @@
+package com.CarRentalSystem;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class Car {
+
+    private UUID id;
+    private String brand;
+    private String model;
+    private String color;
+    private int year;
+    private double price;
+    private boolean available;
+
+    public Car() {}
+}
